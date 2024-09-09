0be0e649fa2dc6cf139735c3cd56439a9a113fae
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-09-09 14:55:53 -0400

    added stelle
    honkai star rail

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getElementById("myH1").textContent = `fortnite`;
+document.getElementById("myH1").textContent = `java`;
+document.getElementById("myH2").textContent = `script`;
+document.getElementById("myH3").textContent = `time`;
+document.getElementById("myH4").textContent = `yay!`;
+
+
+var img = document.createElement("img");
+img.src = "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2023/04/honkai-star-rail-stelle-idle-animation.jpg";
+var src = document.getElementById("header");
+
+document.getElementById("myH5").textContent = `rules are meant to be broken`;
+src.appendChild(img);
